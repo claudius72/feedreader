a49b8bf166691eb348a73a5cfbcb43593af01052
Simona Spataru <simona@simonas-air.aag-de.ibmmobiledemo.com>
2018-04-10 16:39:09 +0300

    created feedreader project

--- src/main/java/com/core/feed/FeedBean.java
+++ src/main/java/com/core/feed/FeedBean.java
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.core.feed;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-/**
- * 
- * @author Gonzalo H. Mendoza
- * email: yogonza524@gmail.com
- * StackOverflow: http://stackoverflow.com/users/5079517/gonza
- */
 @ManagedBean(name="feedBean")
 @RequestScoped
 public class FeedBean {
